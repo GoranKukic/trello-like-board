dd4c5a072fec355b3aeb7c1cb6ce46ebdb0b6dcd
GoranKukic <goranikukic@gmail.com>
2023-03-04 00:37:51 +0100

    Back-end server and REST API finished

--- server/routes/todoItems.js
+++ server/routes/todoItems.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 const todoItemsModel = require('../models/todoItems');
 
-// Creating first route - adding Todo Item to my database
+// Creating first route - adding Todo Item to database
 router.post('/api/item', async (req, res)=>{
     try{
         const newItem = new todoItemsModel({
@@ -20,7 +20,42 @@ router.post('/api/item', async (req, res)=>{
     }
 })
 
+// Creating second route - getting data from database
+
+router.get('/api/items', async (req, res)=>{
+    try{
+        const allTodoItems = await todoItemsModel.find({
+
+        });
+        res.status(200).json(allTodoItems)
+    }catch(err){
+      res.json(err);  
+    }
+})
+
+// Updating item using its id
+router.put('/api/item/:id', async (req, res)=>{
+    try{
+        // finding item by id and updating it
+        const updateItem = await todoItemsModel.findByIdAndUpdate(req.params.id, {$set:req.body});
+        res.status(200).json('Item updated');
+    }catch(err){
+        res.json(err);
+    }
+})
+
+
+// Deleting item from database
+router.delete('/api/item/:id', async (req, res)=>{
+  try{
+    //finding item by id and deleting it
+    const deleteItem = await todoItemsModel.findByIdAndDelete(req.params.id);
+    res.status(200).json('Item Deleted');
+  }catch(err){
+    res.json(err);
+  }
+})
+
 
 // exporting router
-
 module.exports = router;
